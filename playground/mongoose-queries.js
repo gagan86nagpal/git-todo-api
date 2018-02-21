b7e571941a3f6ceeed2cd4156aad0f42ea597d65
gagan86nagpal <gagannagpal68@gmail.com>
2018-02-21 09:32:09 +0530

    Add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,18 +3,18 @@ const {Todo}  = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 const mongoose = require('./../server/db/mongoose');
 const {ObjectID} = require('mongodb');
-var id = "5a8a8b295b88e81a047a9840";
+var id = "5a8b1b6ed49b1831d0f4e7e4";
 
 if(!ObjectID.isValid(id)){
     console.log('Id is not valid');
 }
 
-User.findById(id).then( (user)=>{
-    if(!user){
-        return console.log('User not found');
-    }
-    console.log(user);
-} ).catch((e)=>console.log(e) );
+//User.findById(id).then( (user)=>{
+//    if(!user){
+//        return console.log('User not found');
+//    }
+//    console.log(user);
+//} ).catch((e)=>console.log(e) );
 //Todo.find({
 //    _id:id
 //}).then( (todos)=>{
@@ -33,9 +33,9 @@ User.findById(id).then( (user)=>{
 //    console.log('Todo Found',todo);
 //} ).catch( (e)=>console.log(e) );
 //
-//Todo.findById(id).then( (todo)=>{
-//    if(!todo){
-//       return  console.log('Todo not found');
-//    }
-//    console.log('Todo by ID found',todo);
-//} ).catch( (e)=>console.log(e) );
+Todo.findById(id).then( (todo)=>{
+    if(!todo){
+       return  console.log('Todo not found');
+    }
+    console.log('Todo by ID found',todo);
+} ).catch( (e)=>console.log(e) );
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 var {mongoose}= require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {ObjectId} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json()); // to parse the json from client
@@ -33,6 +33,20 @@ app.get('/todos' , (req,res)=>{
         res.status(400).send(e); // send the error
     } )
 })
+
+app.get('/todos/:id', (req,res)=>{
+    var id = req.params['id'];
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then( (todo)=>{
+        if(!todo){
+            console.log('Empty todo');
+            return res.status(400).send();
+        }
+         res.send({todo});
+    }, (e)=>res.status(400).send());
+} )
 app.listen(3000,()=>{
     console.log(`Started on port 3000`);
 })
